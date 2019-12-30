5efe27e67bc005b8b11aeafc5310f1ea52c03602
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-12-30 13:31:24 -0800

    finished part 3

---
+++ 3-list/js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    let itemInput = document.querySelector("input[name='item-input']");
+    let itemValue = itemInput.value;
+
+    //test to be sure that user entered a value
+    if (itemValue.length !== 0) {
+      //create new item
+      let newItemEl = document.createElement("li");
+      let newCheckBox = document.createElement("input");
+      newCheckBox.setAttribute("type", "checkbox");
+      newItemEl.appendChild(newCheckBox);
+      let newLabel = document.createElement("label");
+      newLabel.textContent = itemValue;
+      newItemEl.appendChild(newLabel);
+
+      //get a reference to list and append list item
+      let list = document.querySelector('.todo-list');
+      list.appendChild(newItemEl);
+      //clear the user input
+      itemInput.value = '';
+      
+      //add a click event listener that serves a toggle
+      newCheckBox.addEventListener("click", function (event) {
+        let doneItem = document.createElement("li");
+        doneItem.textContent = this.nextSibling.textContent;
+        document.getElementById("done-list").appendChild(doneItem);
+        this.parentElement.remove();
+        /*
+        if (this.nextSibling.style.textDecoration == "line-through") {
+          this.nextSibling.style.textDecoration = "";
+        } else {
+          this.nextSibling.style.textDecoration = "line-through";
+        }
+        */
+      });
+      
+    }
+  }) // form submit
+}) // document ready
