db37cf1d3d31212f8f7b6c610ce574c54f006682
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-12-30 11:27:21 -0800

    finished jQuery

--- 2-todo-jquery/js/index.js
+++ 2-todo-jquery/js/index.js
@@ -1,28 +1,31 @@
-//TODO add document ready function
-{
-  //TODO wrap all code related to user submit  
-  {
-    //TODO add code to keep form default submit behavior from occurring
+$(function() {
+  $(document).submit(function(event){
+    event.preventDefault();
+
     // get the value of the input box using jQuery
     //TODO select the DOM object with the name `item-input` and assign its value to a variable called itemValue
+    let itemElement = $("[name='item-input']");
+    let itemValue = itemElement.val();
 
     //check that user has input a value before proceeding
     if (itemValue.length !== 0) {
       //create new item
       // TODO append a new item to the todo list that is a <li> element with two children: an <input> with type checkbox and a <label> with the inner text of the value of itemValue
+      $(".todo-list").append("<li><input type='checkbox'><label>" + itemValue + " </label></li>");
  
-      // TODO clear user input in the input box
-
+      itemElement.val('');
+      
       //set up toggle on check box click
-      // TODO add the code to make this function get added to every :checkbox on the change event
-      (function (event) {
+      $(":checkbox").change(function (event) {
         if ($(this).is(':checked')) {
-          $(this).siblings("label").css("text-decoration", "line-through")
+          $(this).siblings("label").css("text-decoration", "line-through");
         } else {
-          $(this).siblings("label").css("text-decoration", "none")
+          $(this).siblings("label").css("text-decoration", "none");
         }
       }) // checkbox event function
-
+      
     }
+
+    
   }) // form submit
 }) // document ready
